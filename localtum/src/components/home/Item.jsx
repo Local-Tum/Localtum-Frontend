60c0589ea224b64ed96d71dd97382393e9e19a30
[nyun-nye] <[2001_11_7@naver.com]>
2024-08-01 11:38:49 +0900

    Home 에러 수정

--- localtum/src/components/home/Item.jsx
+++ localtum/src/components/home/Item.jsx
@@ -1,143 +1,148 @@
-import React from "react";
-import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
-import openIcon from "../../assets/icons/openIcon.png";
-import closedIcon from "../../assets/icons/closedIcon.png";
-import cafeName from "../../assets/icons/cafeName.png";
-import favoriteButton from "../../assets/icons/favoriteButton.png";
-import favoriteButtonOff from "../../assets/icons/favoriteButtonOff.png";
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import openIcon from '../../assets/icons/openIcon.png';
+import closedIcon from '../../assets/icons/closedIcon.png';
+import cafeName from '../../assets/icons/cafeName.png';
+import favoriteButton from '../../assets/icons/favoriteButton.png';
+import favoriteButtonOff from '../../assets/icons/favoriteButtonOff.png';
 
-const Item = ({ id, name, address, status, image }) => {
-  const navigate = useNavigate();
+const Item = ({ name, address, status, image }) => {
+    const [isFavorite, setIsFavorite] = useState(() => {
+        const favoriteStatus = localStorage.getItem(`favorite-${name}`);
+        return favoriteStatus ? JSON.parse(favoriteStatus) : false;
+    });
 
-  const handleClick = () => {
-    navigate(`/cafedetail/${id}`);
-  };
+    useEffect(() => {
+        localStorage.setItem(`favorite-${name}`, JSON.stringify(isFavorite));
+    }, [isFavorite, name]);
 
-  return (
-    <StyledItem onClick={handleClick}>
-      <ImageContainer>
-        <ItemStatus
-          src={status === "closed" ? closedIcon : openIcon}
-          alt={status === "closed" ? "영업종료" : "영업중"}
-        />
-        <FavoriteButton
-          src={favoriteButton}
-          alt="★"
-          onClick={(e) => e.stopPropagation()}
-        />
-        {image ? (
-          <BackgroundImage src={image} alt="카페 사진" />
-        ) : (
-          <Placeholder>카페 사진</Placeholder>
-        )}
-      </ImageContainer>
-      <ItemDetails>
-        <CafeNameIcon src={cafeName} alt="*" />
-        <ItemName>{name}</ItemName>
-        <ItemDescription>{address}</ItemDescription>
-      </ItemDetails>
-    </StyledItem>
-  );
+    const toggleFavorite = () => {
+        setIsFavorite(!isFavorite);
+    };
+
+    return (
+        <StyledItem>
+            <ImageContainer>
+                <ItemStatus
+                    src={status === 'closed' ? closedIcon : openIcon}
+                    alt={status === 'closed' ? '영업종료' : '영업중'}
+                />
+                <FavoriteButton
+                    src={isFavorite ? favoriteButton : favoriteButtonOff}
+                    alt="★"
+                    onClick={toggleFavorite}
+                />
+                {image ? (
+                    <BackgroundImage src={image} alt="카페 사진" />
+                ) : (
+                    <Placeholder>카페 사진</Placeholder>
+                )}
+            </ImageContainer>
+            <ItemDetails>
+                <CafeNameIcon src={cafeName} alt="*" />
+                <ItemName>{name}</ItemName>
+                <ItemDescription>{address}</ItemDescription>
+            </ItemDetails>
+        </StyledItem>
+    );
 };
 
 const StyledItem = styled.div`
-  overflow: hidden;
-  margin-bottom: 1rem;
-  width: 50vw;
-  cursor: pointer;
-  @media (max-width: 768px) {
-    width: 80vw;
-  }
+    overflow: hidden;
+    margin-bottom: 1rem;
+    width: 50vw;
+    @media (max-width: 768px) {
+        width: 80vw;
+    }
 `;
 
 const ImageContainer = styled.div`
-  border-radius: 30px;
-  position: relative;
-  height: 150px;
-  background-color: #f3f3f3;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    border-radius: 30px;
+    position: relative;
+    height: 150px;
+    background-color: #f3f3f3;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 const BackgroundImage = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  border-radius: 30px;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 30px;
 `;
 
 const Placeholder = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 1rem;
-  font-weight: 400;
-  color: #b5b6b5;
-  background-color: #f3f3f3;
-  border-radius: 30px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1rem;
+    font-weight: 400;
+    color: #b5b6b5;
+    background-color: #f3f3f3;
+    border-radius: 30px;
 `;
 
 const ItemStatus = styled.img`
-  position: absolute;
-  top: 1rem;
-  left: 1rem;
-  z-index: 2;
-  width: 4rem;
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+    z-index: 2;
+    width: 4rem;
 `;
 
 const FavoriteButton = styled.img`
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  background: none;
-  border: none;
-  color: #1c6a00;
-  height: 2.5rem;
-  cursor: pointer;
-  z-index: 2;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    background: none;
+    border: none;
+    color: #1c6a00;
+    height: 2.5rem;
+    cursor: pointer;
+    z-index: 2;
 `;
 
 const ItemDetails = styled.div`
-  padding: 1rem;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+    padding: 1rem;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
 `;
 
 const CafeNameIcon = styled.img`
-  width: 1.5rem;
-  margin-right: 0.5rem;
+    width: 1.5rem;
+    margin-right: 0.5rem;
 `;
 
 const ItemName = styled.h2`
-  color: #282828;
-  font-size: 1.2rem;
-  margin: 0;
-  font-weight: 700;
-  display: flex;
-  align-items: center;
-  @media (max-width: 768px) {
-    font-size: 0.9rem;
-  }
-  @media (max-width: 480px) {
-    font-size: 0.8rem;
-  }
+    color: #282828;
+    font-size: 1.2rem;
+    margin: 0;
+    font-weight: 700;
+    display: flex;
+    align-items: center;
+    @media (max-width: 768px) {
+        font-size: 0.9rem;
+    }
+    @media (max-width: 480px) {
+        font-size: 0.8rem;
+    }
 `;
 
 const ItemDescription = styled.span`
-  color: #808180;
-  font-size: 0.8rem;
-  margin-left: 1rem;
-  @media (max-width: 768px) {
-    font-size: 0.7rem;
-  }
-  @media (max-width: 480px) {
-    font-size: 0.6rem;
-  }
+    color: #808180;
+    font-size: 0.8rem;
+    margin-left: 1rem;
+    @media (max-width: 768px) {
+        font-size: 0.7rem;
+    }
+    @media (max-width: 480px) {
+        font-size: 0.6rem;
+    }
 `;
 
 export default Item;
--- localtum/src/components/home/MainContent.jsx
+++ localtum/src/components/home/MainContent.jsx
@@ -1,149 +1,257 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import locationIcon from "../../assets/icons/locationIcon.png";
-import ItemList from "./ItemList";
-import cafes from "../Cafes/Cafes";
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import locationIcon from '../../assets/icons/locationIcon.png';
+import ItemList from './ItemList';
+import cafes from '../Cafes/Cafes';
 
 const MainContent = () => {
-  const [visibleCount, setVisibleCount] = useState(2);
-  const [district, setDistrict] = useState("위치 찾는 중");
-  const [sortedCafes, setSortedCafes] = useState([]);
+    const [visibleCount, setVisibleCount] = useState(2);
+    const [district, setDistrict] = useState('위치 찾는 중');
+    const [sortedCafes, setSortedCafes] = useState([]);
 
-  useEffect(() => {
-    const apiKey = process.env.REACT_APP_KAKAO_API_KEY;
-    if (!apiKey) {
-      setDistrict("API 키가 설정되지 않았습니다.");
-      return;
-    }
-  }, []);
+    useEffect(() => {
+        const apiKey = process.env.REACT_APP_KAKAO_API_KEY;
+        if (!apiKey) {
+            setDistrict('API 키가 설정되지 않았습니다.');
+            return;
+        }
 
-  return (
-    <StyledMain>
-      <Title>내 주변 카페</Title>
-      <RightContainer>
-        <ToggleButton>
-          <LocationIcon src={locationIcon} alt="locationIcon" />
-          {district}
-        </ToggleButton>
-      </RightContainer>
-      <ItemList items={sortedCafes} visibleCount={visibleCount} />
-      {visibleCount < sortedCafes.length && (
-        <LoadMoreButton
-          onClick={() => setVisibleCount((prevCount) => prevCount + 2)}
-        >
-          더보기
-        </LoadMoreButton>
-      )}
-    </StyledMain>
-  );
+        const script = document.createElement('script');
+        script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${apiKey}&libraries=services&autoload=false`;
+        script.async = true;
+
+        script.onload = () => {
+            window.kakao.maps.load(() => {
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(
+                        position => {
+                            const { latitude, longitude } = position.coords;
+                            fetchDistrictName(latitude, longitude);
+                            calculateDistancesAndStore(latitude, longitude);
+                        },
+                        error => {
+                            console.error('Geolocation error:', error);
+                            setDistrict('위치를 찾을 수 없습니다');
+                        }
+                    );
+                } else {
+                    console.error("Geolocation is not supported by this browser.");
+                    setDistrict('위치를 찾을 수 없습니다');
+                }
+            });
+        };
+
+        script.onerror = () => {
+            console.error('Failed to load the Kakao Map script.');
+            setDistrict('위치를 찾을 수 없습니다');
+        };
+
+        document.head.appendChild(script);
+
+        return () => {
+            document.head.removeChild(script);
+        };
+    }, []);
+
+    const fetchDistrictName = (latitude, longitude) => {
+        const geocoder = new window.kakao.maps.services.Geocoder();
+        const coord = new window.kakao.maps.LatLng(latitude, longitude);
+
+        geocoder.coord2RegionCode(coord.getLng(), coord.getLat(), (result, status) => {
+            if (status === window.kakao.maps.services.Status.OK) {
+                const region = result.find(item => item.region_type === 'H');
+                if (region) {
+                    const districtName = region.address_name.split(' ')[1]; // 구 이름만 추출
+                    setDistrict(districtName);
+                } else {
+                    console.error('No region found for the given coordinates.');
+                    setDistrict('위치를 찾을 수 없습니다.');
+                }
+            } else {
+                console.error('Failed to fetch district name:', status);
+                setDistrict('위치를 찾을 수 없습니다.');
+            }
+        });
+    };
+
+    const calculateDistancesAndStore = (latitude, longitude) => {
+      const updatedCafesList = cafes.map(cafe => {
+          const distance = getDistanceFromLatLonInKm(latitude, longitude, cafe.latitude, cafe.longitude) * 1000;
+          const updatedCafe = { 
+              ...cafe, 
+              distance: Math.round(distance),
+              status: getCafeStatus(cafe) // 새로운 함수로 status 업데이트
+          };
+          localStorage.setItem(`distance-${cafe.id}`, updatedCafe.distance);
+          localStorage.setItem(`status-${cafe.id}`, updatedCafe.status); // 로컬 스토리지에 status 저장
+          return updatedCafe;
+      });
+  
+      const sorted = updatedCafesList.sort((a, b) => a.distance - b.distance);
+      setSortedCafes(sorted);
+  };
+  
+  const getCafeStatus = (cafe) => {
+      const now = new Date();
+      const dayOfWeek = now.getDay();
+      const dayNames = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+      const today = dayNames[dayOfWeek];
+      const currentTime = `${now.getHours()}:${now.getMinutes()}`;
+  
+      if (cafe.hours[today]) {
+          const { open, close, lastOrder } = cafe.hours[today];
+          if (currentTime >= open && currentTime <= lastOrder) {
+              return 'open';
+          }
+      }
+      return 'closed';
+  };
+  
+
+    const getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => {
+        const R = 6371; // Radius of the earth in km
+        const dLat = deg2rad(lat2 - lat1);
+        const dLon = deg2rad(lon1 - lon2);
+        const a =
+            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        const d = R * c; // Distance in km
+        return d;
+    };
+
+    const deg2rad = (deg) => {
+        return deg * (Math.PI / 180);
+    };
+
+    const loadMoreItems = () => {
+        setVisibleCount(prevCount => prevCount + 2);
+    };
+
+    return (
+        <StyledMain>
+            <Title>내 주변 카페</Title>
+            <RightContainer>
+                <ToggleButton>
+                    <LocationIcon src={locationIcon} alt="locationIcon" />
+                    {district}
+                </ToggleButton>
+            </RightContainer>
+            <ItemList items={sortedCafes} visibleCount={visibleCount} />
+            {visibleCount < sortedCafes.length && (
+                <LoadMoreButton onClick={loadMoreItems}>더보기</LoadMoreButton>
+            )}
+        </StyledMain>
+    );
 };
 
 const StyledMain = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 1rem 25vw 5rem 25vw;
-  text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1rem 25vw 5rem 25vw;
+    text-align: center;
 
-  @media (max-width: 768px) {
-    padding: 1rem;
-  }
+    @media (max-width: 768px) {
+        padding: 1rem;
+    }
 
-  @media (max-width: 480px) {
-    padding: 1rem;
-  }
+    @media (max-width: 480px) {
+        padding: 1rem;
+    }
 `;
 
 const Title = styled.h1`
-  font-size: 1.5rem;
-  margin-bottom: 1rem;
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
 
-  @media (max-width: 768px) {
-    font-size: 1.25rem;
-  }
+    @media (max-width: 768px) {
+        font-size: 1.25rem;
+    }
 
-  @media (max-width: 480px) {
-    font-size: 1rem;
-  }
+    @media (max-width: 480px) {
+        font-size: 1rem;
+    }
 `;
 
 const RightContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  width: 100%;
-  margin-bottom: 1rem;
-  padding-right: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    width: 100%;
+    margin-bottom: 1rem;
+    padding-right: 1rem;
 
-  @media (max-width: 768px) {
-    padding-right: 2rem;
-  }
+    @media (max-width: 768px) {
+        padding-right: 2rem;
+    }
 
-  @media (max-width: 480px) {
-    padding-right: 2rem;
-  }
+    @media (max-width: 480px) {
+        padding-right: 2rem;
+    }
 `;
 
 const ToggleButton = styled.button`
-  background-color: #ffffff;
-  border-radius: 15px;
-  border: none;
-  padding: 0.2rem 0.5rem;
-  font-size: 0.8rem;
-  font-weight: 600;
-  color: #467048;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  box-shadow: 0px 0px 12px 2px rgba(0, 0, 0, 0.1);
-  margin-right: 0.5rem;
+    background-color: #ffffff;
+    border-radius: 15px;
+    border: none;
+    padding: 0.2rem 0.5rem;
+    font-size: 0.8rem;
+    font-weight: 600;
+    color: #467048;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0px 0px 12px 2px rgba(0, 0, 0, 0.1);
+    margin-right: 0.5rem;
 
-  @media (max-width: 768px) {
-    font-size: 0.7rem;
-    padding: 0.2rem 0.4rem;
-  }
+    @media (max-width: 768px) {
+        font-size: 0.7rem;
+        padding: 0.2rem 0.4rem;
+    }
 
-  @media (max-width: 480px) {
-    font-size: 0.6rem;
-    padding: 0.2rem 0.3rem;
-  }
+    @media (max-width: 480px) {
+        font-size: 0.6rem;
+        padding: 0.2rem 0.3rem;
+    }
 `;
 
 const LocationIcon = styled.img`
-  height: 0.8rem;
-  cursor: pointer;
-  margin-right: 0.3rem;
+    height: 0.8rem;
+    cursor: pointer;
+    margin-right: 0.3rem;
 
-  @media (max-width: 768px) {
-    height: 0.7rem;
-  }
+    @media (max-width: 768px) {
+        height: 0.7rem;
+    }
 
-  @media (max-width: 480px) {
-    height: 0.6rem;
-  }
+    @media (max-width: 480px) {
+        height: 0.6rem;
+    }
 `;
 
 const LoadMoreButton = styled.button`
-  background-color: #b5b6b5;
-  border-radius: 30px;
-  border: none;
-  padding: 1rem 7rem;
-  font-size: 1rem;
-  color: #fff;
-  text-shadow: 2px 2px 6px rgba(0, 0, 0, 0.25);
-  cursor: pointer;
-  margin-top: 20px;
+    background-color: #b5b6b5;
+    border-radius: 30px;
+    border: none;
+    padding: 1rem 7rem;
+    font-size: 1rem;
+    color: #FFF;
+    text-shadow: 2px 2px 6px rgba(0, 0, 0, 0.25);
+    cursor: pointer;
+    margin-top: 20px;
 
-  @media (max-width: 768px) {
-    padding: 0.8rem 5rem;
-    font-size: 0.875rem;
-  }
+    @media (max-width: 768px) {
+        padding: 0.8rem 5rem;
+        font-size: 0.875rem;
+    }
 
-  @media (max-width: 480px) {
-    padding: 0.6rem 3rem;
-    font-size: 0.75rem;
-  }
+    @media (max-width: 480px) {
+        padding: 0.6rem 3rem;
+        font-size: 0.75rem;
+    }
 `;
 
 export default MainContent;
